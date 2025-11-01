837358aa55d6e5837ce2c4c85046f6d45f6473b4
Paula Gearon <gearon@gmail.com>
2025-11-01 10:58:51 -0400

    Refactored to perform Cuda operations in a function

--- cuda.cu
+++ cuda.cu
@@ -30,11 +30,9 @@ __global__ void kNN(int* predictions,  // the result
   if (tid < test_num_instances) {
     float* sharedCandidates = (float*)smem;
     float* candidates = sharedCandidates + (threadIdx.x * 2 * k);
-    // float* candidates = (float*)malloc(2 * k * sizeof(float));
 
     int* sharedClassCounts = (int*)(smem + (blockDim.x * 2 * k * sizeof(float)));
     int* classCounts = sharedClassCounts + threadIdx.x * num_classes;
-    // int* classCounts = (int*)malloc(num_classes * sizeof(int));
 
     for (int i = 0; i < k * 2; i++) candidates[i] = FLT_MAX;
     for (int i = 0; i < num_classes; i++) classCounts[i] = 0;
@@ -98,110 +96,131 @@ float computeAccuracy(int* confusionMatrix, ArffData* dataset) {
     return 100 * successfulPredictions / (float) dataset->num_instances();
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("Usage: %s datasets/train.arff datasets/test.arff k\n", argv[0]);
-        exit(0);
-    }
-
-    // k value for the k-nearest neighbors
-    int k = strtol(argv[3], NULL, 10);
-    if (errno != 0) {
-      printf("k value must be an integer. Got '%s'\n", argv[3]);
-      exit(1);
-    }
-
-    // Open the datasets
-    ArffParser parserTest(argv[2]);
-    ArffParser parserTrain(argv[1]);
-    ArffData *test = parserTest.parse();   // owned and freed by parserTest
-    ArffData *train = parserTrain.parse(); // owned and freed by parserTrain
-
-    // extract the data for passing to the device
-    float* h_test_matrix = test->get_dataset_matrix();
-    float* h_train_matrix = train->get_dataset_matrix();
-
-    int num_attributes = train->num_attributes();
-    int test_num_instances = test->num_instances();
-    int train_num_instances = train->num_instances();
-    int num_classes = train->num_classes();
-
-    // allocate host memory for the result
-    int resultSize = test_num_instances * sizeof(int);
-    int* h_predictions = (int*)malloc(resultSize);
-
-    // allocate memory on the device
-    float *d_test_matrix, *d_train_matrix;
-    int testElements = test_num_instances * num_attributes;
-    int trainElements = train_num_instances * num_attributes;
-    cudaMalloc(&d_test_matrix, testElements * sizeof(float));
-    cudaMalloc(&d_train_matrix, trainElements * sizeof(float));
-
-    int *d_predictions;
-    cudaMalloc(&d_predictions, resultSize);
-
-    // copy data to the device
-    cudaMemcpy(d_test_matrix, h_test_matrix, testElements * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_train_matrix, h_train_matrix, trainElements * sizeof(float), cudaMemcpyHostToDevice);
-
-    // determine the amount of shared memory available per block
-    int device;
-    cudaGetDevice(&device);
-    int maxOpt = 0, maxDefault = 0, maxThreadsPB = 0;
-    cudaDeviceGetAttribute(&maxOpt, cudaDevAttrMaxSharedMemoryPerBlockOptin, device);
-    cudaDeviceGetAttribute(&maxDefault, cudaDevAttrMaxSharedMemoryPerBlock, device);
-    cudaDeviceGetAttribute(&maxThreadsPB, cudaDevAttrMaxThreadsPerBlock, device);
-    int maxShm = maxOpt ? maxOpt : maxDefault;
-
-    // set up timing
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    float milliseconds = 0;
-
-    // 1 thread per test. Group into blocks of 256
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (test_num_instances + threadsPerBlock - 1) / threadsPerBlock;
-    int sharedMemorySize = threadsPerBlock * (2 * k * sizeof(float) + num_classes * sizeof(int));
-    if (sharedMemorySize > maxShm) {
-      printf("Requires too much shared memory per block. Required = %d. Available = %d\n", sharedMemorySize, maxShm);
-      exit(2);
-    }
-    printf("Max shared memory = %d bytes.\n", maxShm);
-    printf("%d blocks of %d threads. %d bytes of shared memory.\n", blocksPerGrid, threadsPerBlock, sharedMemorySize);
-
-    // start timer
-    cudaEventRecord(start);
-
-    kNN<<<blocksPerGrid, threadsPerBlock, sharedMemorySize>>>(d_predictions, d_test_matrix, d_train_matrix, test_num_instances, train_num_instances,
-                                                              num_attributes, num_classes, k);
-
-    cudaError_t err = cudaGetLastError();
-
-    if (err != cudaSuccess) {
-      printf("CUDA Error: %s\n", cudaGetErrorString(err));
-    } else {
-      // end timer
-      cudaEventRecord(stop);
-      cudaEventSynchronize(stop);
-      cudaEventElapsedTime(&milliseconds, start, stop);
-
-      // retrieve the result
-      cudaMemcpy(h_predictions, d_predictions, resultSize, cudaMemcpyDeviceToHost);
-
-      // Compute the confusion matrix
-      int* confusionMatrix = computeConfusionMatrix(h_predictions, test);
-      // Calculate the accuracy
-      float accuracy = computeAccuracy(confusionMatrix, test);
-
-      printf("The %i-NN classifier for %u test instances and %u train instances required %f ms GPU time for GPU. Accuracy was %.2f%%\n",
-             k, test_num_instances, train_num_instances, milliseconds, accuracy);
-
-      free(confusionMatrix);
-    }
-
-    cudaFree(d_test_matrix);
-    cudaFree(d_train_matrix);
-    cudaFree(d_predictions);
-    free(h_predictions);
+// Queries the device for the maximum shared memory available in a block.
+// We know this is large enough (over 100kb) but it's good to check
+int maxCudaSharedMemory() {
+  int device;
+  cudaGetDevice(&device);
+  int maxOpt = 0, maxDefault = 0, maxThreadsPB = 0;
+  cudaDeviceGetAttribute(&maxOpt, cudaDevAttrMaxSharedMemoryPerBlockOptin, device);
+  cudaDeviceGetAttribute(&maxDefault, cudaDevAttrMaxSharedMemoryPerBlock, device);
+  cudaDeviceGetAttribute(&maxThreadsPB, cudaDevAttrMaxThreadsPerBlock, device);
+  return maxOpt ? maxOpt : maxDefault;
 }
+
+// Perform the Cuda setup and calling. Requires a result array that will be filled in.
+// Returns the milliseconds for execution.
+float hostKNN(int* h_results, int resultSizeInBytes,            // result matrix to fill in
+              float* h_test_matrix, float* h_train_matrix,      // data matrices
+              int test_num_instances, int train_num_instances,  // limits of data matrices
+              int num_attributes,                               // dimensions
+              int num_classes, int k) {
+
+  // allocate memory on the device
+  float *d_test_matrix, *d_train_matrix;
+  int testElements = test_num_instances * num_attributes;
+  int trainElements = train_num_instances * num_attributes;
+  cudaMalloc(&d_test_matrix, testElements * sizeof(float));
+  cudaMalloc(&d_train_matrix, trainElements * sizeof(float));
+
+  int *d_predictions;
+  cudaMalloc(&d_predictions, resultSizeInBytes);
+
+  // copy data to the device
+  cudaMemcpy(d_test_matrix, h_test_matrix, testElements * sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_train_matrix, h_train_matrix, trainElements * sizeof(float), cudaMemcpyHostToDevice);
+
+  // determine the amount of shared memory available per block
+  // set up timing
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  float milliseconds = 0;
+
+  // 1 thread per test. Group into blocks of 256
+  int threadsPerBlock = 256;
+  int blocksPerGrid = (test_num_instances + threadsPerBlock - 1) / threadsPerBlock;
+  int sharedMemorySize = threadsPerBlock * (2 * k * sizeof(float) + num_classes * sizeof(int));
+  int maxShm = maxCudaSharedMemory();
+  if (sharedMemorySize > maxShm) {
+    printf("Requires too much shared memory per block. Required = %d. Available = %d\n", sharedMemorySize, maxShm);
+    exit(2);
+  }
+  printf("%d blocks of %d threads.\n", blocksPerGrid, threadsPerBlock);
+
+  // start timer
+  cudaEventRecord(start);
+
+  kNN<<<blocksPerGrid, threadsPerBlock, sharedMemorySize>>>(d_predictions, d_test_matrix, d_train_matrix, test_num_instances, train_num_instances,
+                                                            num_attributes, num_classes, k);
+
+  cudaError_t err = cudaGetLastError();
+
+  if (err != cudaSuccess) {
+    printf("CUDA Error: %s\n", cudaGetErrorString(err));
+    exit(3);
+  }
+  // end timer
+  cudaEventRecord(stop);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&milliseconds, start, stop);
+
+  // retrieve the result
+  cudaMemcpy(h_results, d_predictions, resultSizeInBytes, cudaMemcpyDeviceToHost);
+
+  // clean up the device resources
+  cudaFree(d_test_matrix);
+  cudaFree(d_train_matrix);
+  cudaFree(d_predictions);
+
+  // Results are in the provided result array
+  return milliseconds;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 4) {
+      printf("Usage: %s datasets/train.arff datasets/test.arff k\n", argv[0]);
+      exit(0);
+  }
+
+  // k value for the k-nearest neighbors
+  int k = strtol(argv[3], NULL, 10);
+  if (errno != 0) {
+    printf("k value must be an integer. Got '%s'\n", argv[3]);
+    exit(1);
+  }
+
+  // Open the datasets
+  ArffParser parserTest(argv[2]);
+  ArffParser parserTrain(argv[1]);
+  ArffData *test = parserTest.parse();   // owned and freed by parserTest
+  ArffData *train = parserTrain.parse(); // owned and freed by parserTrain
+
+  int test_num_instances = test->num_instances();
+  int train_num_instances = train->num_instances();
+
+  // Allocate host memory for the result.
+  // Allocation happens here because we're freeing here.
+  int resultSizeInBytes = test_num_instances * sizeof(int);
+  int* results = (int*)malloc(resultSizeInBytes);
+
+  float milliseconds = hostKNN(results, resultSizeInBytes,
+                               test->get_dataset_matrix(), train->get_dataset_matrix(),
+                               test_num_instances, train_num_instances,
+                               train->num_attributes(), train->num_classes(), k);
+
+  // Compute the confusion matrix
+  int* confusionMatrix = computeConfusionMatrix(results, test);
+  // Calculate the accuracy
+  float accuracy = computeAccuracy(confusionMatrix, test);
+
+  printf("The %i-NN classifier for %u test instances and %u train instances required %f ms GPU time for GPU. Accuracy was %.2f%%\n",
+           k, test_num_instances, train_num_instances, milliseconds, accuracy);
+
+  free(confusionMatrix);
+
+  free(results);
+
+  return 0;
+}
+
